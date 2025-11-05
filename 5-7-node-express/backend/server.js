6a30e35d31c3108fcf36747082290823ef70cb73
Abdulrahman Alhendawi <a-rh2013@hotmail.com>
2025-11-05 09:05:42 +0300

    TODO4 done

--- 5-7-node-express/backend/server.js
+++ 5-7-node-express/backend/server.js
@@ -6,7 +6,18 @@ import { getRandomQuote } from "./quotes.js";
 const app = express();
 const PORT = 3000;
 
-// Note: CORS and routes are implemented in later TODOs
+// Enable CORS so the frontend (running on a different port) can call this API
+app.use(cors());
+
+// Define routes
+app.get("/", (req, res) => {
+	res.send("Welcome to the Quote Generator API");
+});
+
+app.get("/api/quote", (req, res) => {
+	const quote = getRandomQuote();
+	res.json({ quote });
+});
 
 // Start the server
 app.listen(PORT, () => {
